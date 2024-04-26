97281456060c97ed392c0477fd6935b77c5032a2
ammmar <ammaraslam7164@gmail.com>
2024-04-26 12:26:12 +0600

    scrollbar-added-index

--- src/App.jsx
+++ src/App.jsx
@@ -8,33 +8,38 @@ import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import ScrollToTop from "react-scroll-to-top";
 
-
 function App() {
   return (
-   
-    <div>
-       
-      <div className=""> <Navbar></Navbar></div>
-     
-      <div className="max-w-6xl mx-auto">
-        <Header></Header>
-        <About></About>
-        <Services></Services>
-        <Portfolio></Portfolio>
-        <Contact></Contact>
-      </div>
-      <Footer></Footer>
-      <ScrollToTop  className="bg-[#00eeff] hidden lg:flex items-center justify-center " color="black" style={{
-  width: '50px',
-  height: '50px',
-  borderRadius: '50%',
-  boxShadow: '0 0px 30px #00eeff',
-  
-  // Add more styles as needed
-}} smooth />
+    
+      <div>
+        <div className="">
+          {" "}
+          <Navbar></Navbar>
+        </div>
 
-    </div>
-  
+        <div className="max-w-6xl mx-auto">
+          <Header></Header>
+          <About></About>
+          <Services></Services>
+          <Portfolio></Portfolio>
+          <Contact></Contact>
+        </div>
+        <Footer></Footer>
+        <ScrollToTop
+          className="bg-[#00eeff] hidden lg:flex items-center justify-center "
+          color="black"
+          style={{
+            width: "50px",
+            height: "50px",
+            borderRadius: "50%",
+            boxShadow: "0 0px 30px #00eeff",
+
+            // Add more styles as needed
+          }}
+          smooth
+        />
+      </div>
+ 
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-
   daisyui: {
-    themes: ["light", "dark", "cupcake","synthwave"],
+    themes: ["light", "dark", "cupcake", "synthwave"],
   },
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      plugins: [
+        // ...
+        require("tailwind-scrollbar"),
+      ],
     },
-           
-      fontFamily:{
-        
-        signature :["Great Vibes"],
 
-      
-
+    fontFamily: {
+      signature: ["Great Vibes"],
     },
   },
   plugins: [require("daisyui")],
-}
-
+};
