e9e569353541efce7ce3c8cce562ae29f6b91c17
ammmar <ammaraslam7164@gmail.com>
2024-04-26 11:13:19 +0600

    fixed-get-in-touch

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -31,23 +31,24 @@ const Contact = () => {
       </h2>
       <div>
       <form onSubmit={handleSubmitMessage} className="card-body w-1/2 mx-auto">
+        <h2 className="text-white mb-10">Feel free to inquire or connect via my social accounts for any queries..</h2>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Name</span>
           </label>
-          <input type="name" name="name" placeholder="Name" className="input input-bordered hover:outline hover:outline-[#00eeff] outline outline-1 text-white" required />
+          <input type="name" name="name" placeholder="Your Name" className="input input-bordered hover:outline hover:outline-[#00eeff] outline outline-1 text-white" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" name="email" placeholder="Email" className="input input-bordered hover:outline hover:outline-[#00eeff] outline outline-1  text-white" required />
+          <input type="email" name="email" placeholder="Email Address" className="input input-bordered hover:outline hover:outline-[#00eeff] outline outline-1  text-white" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Messaage</span>
           </label>
-          <textarea name="message" className="input input-bordered border   hover:outline hover:outline-[#00eeff] outline outline-1 h-24 text-white"></textarea>
+          <textarea placeholder="Your Message" name="message" className="input input-bordered border   hover:outline hover:outline-[#00eeff] outline outline-1 h-24 text-white"></textarea>
          
         </div>
         <div className="form-control mt-6">
